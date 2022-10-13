58e7a1fe1a1a27f8cf4b98cd47dd1db15954d571
Michelle Mujica <CltControl@GGIECUT2WRK064.espol.edu.ec>
2022-10-13 18:27:41 -0500

    Creation of Class CarInsurance

---
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -0,0 +1,34 @@
+package ec.edu.espol.workshops;
+
+public class CarInsurance {
+	private int age;
+	private char sex;
+	private boolean isMarried, isValidLicense;
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public char getSex() {
+		return sex;
+	}
+	public void setSex(char sex) {
+		this.sex = sex;
+	}
+	public boolean isMarried() {
+		return isMarried;
+	}
+	public void setMarried(boolean isMarried) {
+		this.isMarried = isMarried;
+	}
+	public boolean isValidLicense() {
+		return isValidLicense;
+	}
+	public void setValidLicense(boolean isValidLicense) {
+		this.isValidLicense = isValidLicense;
+	}
+	
+}
+
+
