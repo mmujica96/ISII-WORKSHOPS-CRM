1c648f67ca51ab17897b787da879e041651b43f2
Michelle Mujica <mmujica@espol.edu.ec>
2022-10-21 23:09:20 -0500

    Checkstyle

--- ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
+++ ec.edu.espol.workshops/lib/src/main/java/ec/edu/espol/workshops/CarInsurance.java
@@ -8,17 +8,13 @@ package ec.edu.espol.workshops;
  *
  */
 public class CarInsurance {
-	//customer instance
 	private Customer customer;
 	//The base premium is $500, it must be applied to every insurance;
 	final int basePremium = 500; 
-	
 	public void setCustomer(Customer customer) {
 		this.customer = customer;
 	}
-	
-	public Customer getCustomer() {
-		return this.customer;
+	public Customer getCustomer() {return this.customer;
 	}
 	
 	/**
@@ -30,8 +26,7 @@ public class CarInsurance {
 		int age = customer.getAge();
 		char sex = customer.getSex();
 		boolean married = customer.isMarried();
-		
-		if( age > 80) {
+		if(customer.isValidLicense() !=true || age > 80){
 			//si es mayor de 80 no puede ser asegurado
 			// si no tiene licencia no es asegurado
 			return -1;
@@ -39,22 +34,22 @@ public class CarInsurance {
 		
 		//- If the customer is male, not married, and younger than 25, 
 		//  then it is needed to add $1500 to the base premium;
-		if(sex == 'M') {
+		if(sex == 'M'){
 			if(!married && age < 25)
 			premium = 1500 + this.basePremium;
 			
 		//- If the customer is female, or if the customer is married, 
 		//  then it is needed to subtract $200 from the base premium;
-		}else if(sex == 'F') {
+		}else if(sex == 'F'){
 			if(married)
 			premium = this.basePremium - 200;
-		}else {
+		}else{
 			return -1; // solo puede ser hombre o mujer
 		}
 		
 		//If the customer is 45 or older, but younger than 65, 
 		//then it is needed to subtract $100 from the base premium.
-		if(age >= 45 && age < 65) {
+		if(age >= 45 && age < 65){
 			premium = this.basePremium - 100;
 		}
 
